1eb80c50fe0f524746852860374df2da959a3cc9
lucasolv505 <155476756+tridentam@users.noreply.github.com>
2024-12-27 15:34:31 -0300

    favicon e title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/techlogo.png"/>
+        <title>Next + TS + Store</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
